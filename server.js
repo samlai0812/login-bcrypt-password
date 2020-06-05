2de7aba4e7e9074b73d1a2242a44102a9bbd35ab
Sam Lai <samlai0812@gmail.com>
2020-06-05 17:34:45 +0800

    fist commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const app = express()
+app.use(express.json())
+const users = []
+app.get("/users",(req, res)=>{
+    res.json(users)
+})
+app.post("/users",async (req, res)=>{
+    try{
+        const hashedpassword = await bcrypt.hash(req.body.password,10)
+        user = {name : req.body.name, password : hashedpassword}
+        users.push(user)
+        res.status(201).send()
+    }catch{
+        res.status(500).send()
+    }
+})
+
+app.post("/users/login",async (req, res)=>{
+    const user = users.find( (user) => user.name === req.body.name )
+    if(user == null){
+        return res.status(401).send('Can not find this user')
+    }
+    try{
+        if(await bcrypt.compare(req.body.password, user.password )){
+            res.send('Login in')
+        }else{
+            res.send('Not Allowed')
+        }
+    }catch{
+            res.status(500).send()
+        }
+})
+app.listen(3000)
